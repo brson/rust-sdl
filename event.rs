e67d32ed81849df61920296169499c9e7d1a8c0b
Tony Aldridge <zaragopha@hotmail.com>
2012-12-16 19:57:22 +0000

    Working on Events and Keyboard

--- event.rs
+++ event.rs
@@ -1,41 +1,5 @@
 use libc::c_int;
 
-type EventType = u8;
-
-const noevent: u8 = 0u8;
-const activeevent: u8 = 1u8;
-const keydown: u8 = 2u8;
-const keyup: u8 = 3u8;
-const mousemotion: u8 = 4u8;
-const mousebuttondown: u8 = 5u8;
-const mousebuttonup: u8 = 6u8;
-const joyaxismotion: u8 = 7u8;
-const joyballmotion: u8 = 8u8;
-const joyhatmotion: u8 = 9u8;
-const joybuttondown: u8 = 10u8;
-const joybuttonup: u8 = 11u8;
-const quit: u8 = 12u8;
-const syswmevent: u8 = 13u8;
-const event_reserveda: u8 = 14u8;
-const event_reservedb: u8 = 15u8;
-const videoresize: u8 = 16u8;
-const videoexpose: u8 = 17u8;
-const event_reserved2: u8 = 18u8;
-const event_reserved3: u8 = 19u8;
-const event_reserved4: u8 = 20u8;
-const event_reserved5: u8 = 21u8;
-const event_reserved6: u8 = 22u8;
-const event_reserved7: u8 = 23u8;
-const event_userevent: u8 = 24u8;
-
-type RawEvent = {
-    type_: EventType,
-    // FIXME: Not sure exactly how big this needs to be
-    event: (u64, u64, u64, u64, u64, u64, u64, u64,
-            u64, u64, u64, u64, u64, u64, u64, u64,
-            u64, u64, u64, u64, u64, u64, u64, u64)
-};
-
 pub enum Event {
     pub KeyDownEvent(*KeyboardEvent_),
     pub KeyUpEvent(*KeyboardEvent_),
@@ -44,8 +8,8 @@ pub enum Event {
 }
 
 impl Event: cmp::Eq {
-    pure fn eq(other: &Event) -> bool {
-        match (self, *other) {
+    pure fn eq(&self, other: &Event) -> bool {
+        match (*self, *other) {
             (QuitEvent, QuitEvent) | (NoEvent, NoEvent) => true,
             (KeyDownEvent(left), KeyDownEvent(right)) |
             (KeyUpEvent(left), KeyUpEvent(right)) => {
@@ -54,27 +18,23 @@ impl Event: cmp::Eq {
             _ => false
         }
     }
-    pure fn ne(other: &Event) -> bool {
+    pure fn ne(&self, other: &Event) -> bool {
         !self.eq(other)
     }
 }
 
-
 type QuitEvent_ = {
-    type_: EventType 
+    type_: ll::event::sdl_event_type
 };
 
-const SDL_RELEASED: u8 = 0u8;
-const SDL_PRESSED: u8 = 1u8;
-
 type KeyboardEvent_ = {
-    type_: EventType,
+    type_: ll::event::sdl_event_type,
     which: u8,
     state: u8,
     keysym: keyboard::KeySym
 };
 
-fn null_event() -> RawEvent {
+fn null_event() -> ll::event::Event {
     {
         type_: 0u8,
         event: (0u64, 0u64, 0u64, 0u64, 0u64, 0u64, 0u64, 0u64,
@@ -83,34 +43,34 @@ fn null_event() -> RawEvent {
     }
 }
 
-pub fn log_event(e: &RawEvent) {
-    if e.type_ == noevent { return }
-    let name = if e.type_ == noevent { ~"none" }
-    else if e.type_ == activeevent { ~"active" }
-    else if e.type_ == keydown { ~"keydown" }
-    else if e.type_ == keyup { ~"keyup" }
-    else if e.type_ == mousemotion { ~"mousemotion" }
-    else if e.type_ == mousebuttondown { ~"mousebuttondown" }
-    else if e.type_ == mousebuttonup { ~"mousebuttonup" }
-    else if e.type_ == quit { ~"quit" }
-    else if e.type_ == syswmevent { ~"syswmevent" }
-    else if e.type_ == videoresize { ~"videoresize" }
-    else if e.type_ == videoexpose { ~"videoexpose" }
+fn log_event(e: &ll::event::Event) {
+    if e.type_ == ll::event::SDL_NOEVENT { return }
+    let name = if e.type_ == ll::event::SDL_NOEVENT { ~"none" }
+    else if e.type_ == ll::event::SDL_ACTIVEEVENT { ~"active" }
+    else if e.type_ == ll::event::SDL_KEYDOWN { ~"keydown" }
+    else if e.type_ == ll::event::SDL_KEYUP { ~"keyup" }
+    else if e.type_ == ll::event::SDL_MOUSEMOTION { ~"mousemotion" }
+    else if e.type_ == ll::event::SDL_MOUSEBUTTONDOWN { ~"mousebuttondown" }
+    else if e.type_ == ll::event::SDL_MOUSEBUTTONUP { ~"mousebuttonup" }
+    else if e.type_ == ll::event::SDL_QUIT { ~"quit" }
+    else if e.type_ == ll::event::SDL_SYSWMEVENT { ~"syswmevent" }
+    else if e.type_ == ll::event::SDL_VIDEORESIZE { ~"videoresize" }
+    else if e.type_ == ll::event::SDL_VIDEOEXPOSE { ~"videoexpose" }
     else { ~"other" };
-    #debug("event: %s", name);
+    debug!("event: %s", name);
 }
 
 pub fn poll_event(f: fn(Event)) unsafe {
     let raw_event = null_event();
-    let result = SDL::SDL_PollEvent(ptr::addr_of(&raw_event));
+    let result = ll::event::SDL_PollEvent(ptr::addr_of(&raw_event));
     if result as int == 1 {
         let event_ptr = ptr::addr_of(&raw_event.event);
         log_event(&raw_event);
-        if (raw_event.type_ == quit) {
+        if (raw_event.type_ == ll::event::SDL_QUIT) {
             f(QuitEvent);
-        } else if (raw_event.type_ == keydown) {
+        } else if (raw_event.type_ == ll::event::SDL_KEYDOWN) {
             f(KeyDownEvent(cast::reinterpret_cast(&event_ptr)));
-        } else if (raw_event.type_ == keyup) {
+        } else if (raw_event.type_ == ll::event::SDL_KEYUP) {
             f(KeyUpEvent(cast::reinterpret_cast(&event_ptr)));
         } else {
             f(NoEvent);
@@ -118,8 +78,4 @@ pub fn poll_event(f: fn(Event)) unsafe {
     } else {
         f(NoEvent);
     }
-}
-
-extern mod SDL {
-    fn SDL_PollEvent(event: *RawEvent) -> c_int;
 }
--- ll.rs
+++ ll.rs
@@ -53,42 +53,344 @@ pub mod sdl {
 
 pub mod event {
 
-    type sdl_event_type = u8; //FIXME: Should this be c_enum?
+    pub type sdl_button_state = u8;
 
-    const SDL_NOEVENT: sdl_event_type = 0u8;
-    const SDL_ACTIVEEVENT: sdl_event_type = 1u8;
-    const SDL_KEYDOWN: sdl_event_type = 2u8;
-    const SDL_KEYUP: sdl_event_type = 3u8;
-    const SDL_MOUSEMOTION: sdl_event_type = 4u8;
-    const SDL_MOUSEBUTTONDown: sdl_event_type = 5u8;
-    const SDL_MOUSEBUTTONUP: sdl_event_type = 6u8;
-    const SDL_JOYAXISMOTION: sdl_event_type = 7u8;
-    const SDL_JOYBALLMOTION: sdl_event_type = 8u8;
-    const SDL_JOYHATMOTION: sdl_event_type = 9u8;
-    const SDL_JOYBUTTONDOWN: sdl_event_type = 10u8;
-    const SDL_JOYBUTTONUP: sdl_event_type = 11u8;
-    const SDL_QUIT: sdl_event_type = 12u8;
-    const SDL_SYSWMEVENT: sdl_event_type = 13u8;
-    const SDL_EVENT_RESERVEDA: sdl_event_type = 14u8;
-    const SDL_EVENT_RESERVEDB: sdl_event_type = 15u8;
-    const SDL_VIDEORESIZE: sdl_event_type = 16u8;
-    const SDL_VIDEOEXPOSE: sdl_event_type = 17u8;
-    const SDL_EVENT_RESERVED2: sdl_event_type = 18u8;
-    const SDL_EVENT_RESERVED3: sdl_event_type = 19u8;
-    const SDL_EVENT_RESERVED4: sdl_event_type = 20u8;
-    const SDL_EVENT_RESERVED5: sdl_event_type = 21u8;
-    const SDL_EVENT_RESERVED6: sdl_event_type = 22u8;
-    const SDL_EVENT_RESERVED7: sdl_event_type = 23u8;
-    const SDL_EVENT_USEREVENT: sdl_event_type = 24u8;
+    pub const SDL_RELEASED: sdl_button_state = 0u8;
+    pub const SDL_PRESSED: sdl_button_state = 1u8;
+
+    pub type sdl_event_type = u8;
+
+    pub const SDL_NOEVENT: sdl_event_type = 0u8;
+    pub const SDL_ACTIVEEVENT: sdl_event_type = 1u8;
+    pub const SDL_KEYDOWN: sdl_event_type = 2u8;
+    pub const SDL_KEYUP: sdl_event_type = 3u8;
+    pub const SDL_MOUSEMOTION: sdl_event_type = 4u8;
+    pub const SDL_MOUSEBUTTONDOWN: sdl_event_type = 5u8;
+    pub const SDL_MOUSEBUTTONUP: sdl_event_type = 6u8;
+    pub const SDL_JOYAXISMOTION: sdl_event_type = 7u8;
+    pub const SDL_JOYBALLMOTION: sdl_event_type = 8u8;
+    pub const SDL_JOYHATMOTION: sdl_event_type = 9u8;
+    pub const SDL_JOYBUTTONDOWN: sdl_event_type = 10u8;
+    pub const SDL_JOYBUTTONUP: sdl_event_type = 11u8;
+    pub const SDL_QUIT: sdl_event_type = 12u8;
+    pub const SDL_SYSWMEVENT: sdl_event_type = 13u8;
+    pub const SDL_EVENT_RESERVEDA: sdl_event_type = 14u8;
+    pub const SDL_EVENT_RESERVEDB: sdl_event_type = 15u8;
+    pub const SDL_VIDEORESIZE: sdl_event_type = 16u8;
+    pub const SDL_VIDEOEXPOSE: sdl_event_type = 17u8;
+    pub const SDL_EVENT_RESERVED2: sdl_event_type = 18u8;
+    pub const SDL_EVENT_RESERVED3: sdl_event_type = 19u8;
+    pub const SDL_EVENT_RESERVED4: sdl_event_type = 20u8;
+    pub const SDL_EVENT_RESERVED5: sdl_event_type = 21u8;
+    pub const SDL_EVENT_RESERVED6: sdl_event_type = 22u8;
+    pub const SDL_EVENT_RESERVED7: sdl_event_type = 23u8;
+    pub const SDL_EVENT_USEREVENT: sdl_event_type = 24u8;
+
+    pub type Event = {
+        type_: sdl_event_type,
+        // FIXME: Not sure exactly how big this needs to be
+        event: (u64, u64, u64, u64, u64, u64, u64, u64,
+                u64, u64, u64, u64, u64, u64, u64, u64,
+                u64, u64, u64, u64, u64, u64, u64, u64)
+    };
 
     extern {
-        fn SDL_PollEvent(event: *RawEvent) -> c_int;
+        fn SDL_PollEvent(event: *Event) -> c_int;
     }
 }
-/*
+
 pub mod keyboard {
+    // sym corresponds to the `Key` enum, mod_ to the `Mod` enum. We should
+    // be using the correct type here but our enums don't have the right
+    // size yet
+    pub type KeySym = {
+        scancode: u8,
+        sym: c_int,
+        mod_: c_int,
+        unicode: u16
+    };
 }
-*/
+
+pub mod keysym {
+    
+    /** What we really want is a mapping of every raw key on the keyboard.
+     *  To support international keyboards, we use the range 0xA1 - 0xFF
+     *  as international virtual keycodes.  We'll follow in the footsteps of X11...
+     *  @brief The names of the keys
+     */
+    pub type sdl_key_type = c_int;
+        /** @name ASCII mapped KeySyms
+        *  The keyboard syms have been cleverly chosen to map to ASCII
+        */
+        /*@{*/
+        pub SDLK_UNKNOWN         = 0,
+        //pub SDLK_First         = 0,
+        pub SDLK_BACKSPACE       = 8,
+        pub SDLK_TAB             = 9,
+        pub SDLK_CLEAR           = 12,
+        pub SDLK_RETURN          = 13,
+        pub SDLK_PAUSE           = 19,
+        pub SDLK_ESCAPE          = 27,
+        pub SDLK_SPACE           = 32,
+        pub SDLK_EXCLAIM         = 33,
+        pub SDLK_QUOTEDBL        = 34,
+        pub SDLK_HASH            = 35,
+        pub SDLK_DOLLAR          = 36,
+        pub SDLK_AMPERSAND       = 38,
+        pub SDLK_QUOTE           = 39,
+        pub SDLK_LEFTPAREN       = 40,
+        pub SDLK_RIGHTPAREN      = 41,
+        pub SDLK_ASTERISK        = 42,
+        pub SDLK_PLUS            = 43,
+        pub SDLK_COMMA           = 44,
+        pub SDLK_MINUS           = 45,
+        pub SDLK_PERIOD          = 46,
+        pub SDLK_SLASH           = 47,
+        pub SDLK_0               = 48,
+        pub SDLK_1               = 49,
+        pub SDLK_2               = 50,
+        pub SDLK_3               = 51,
+        pub SDLK_4               = 52,
+        pub SDLK_5               = 53,
+        pub SDLK_6               = 54,
+        pub SDLK_7               = 55,
+        pub SDLK_8               = 56,
+        pub SDLK_9               = 57,
+        pub SDLK_COLON           = 58,
+        pub SDLK_SEMICOLON       = 59,
+        pub SDLK_LESS            = 60,
+        pub SDLK_EQUALS          = 61,
+        pub SDLK_GREATER         = 62,
+        pub SDLK_QUESTION        = 63,
+        pub SDLK_AT              = 64,
+        /* 
+        Skip uppercase letters
+        */
+        pub SDLK_LEFTBRACKET     = 91,
+        pub SDLK_BACKSLASH       = 92,
+        pub SDLK_RIGHTBRACKEt    = 93,
+        pub SDLK_CARET           = 94,
+        pub SDLK_UNDERSCORE      = 95,
+        pub SDLK_BACKQUOTE       = 96,
+        pub SDLK_A               = 97,
+        pub SDLK_B               = 98,
+        pub SDLK_C               = 99,
+        pub SDLK_D               = 100,
+        pub SDLK_E               = 101,
+        pub SDLK_F               = 102,
+        pub SDLK_G               = 103,
+        pub SDLK_H               = 104,
+        pub SDLK_I               = 105,
+        pub SDLK_J               = 106,
+        pub SDLK_K               = 107,
+        pub SDLK_L               = 108,
+        pub SDLK_M               = 109,
+        pub SDLK_N               = 110,
+        pub SDLK_O               = 111,
+        pub SDLK_P               = 112,
+        pub SDLK_Q               = 113,
+        pub SDLK_R               = 114,
+        pub SDLK_S               = 115,
+        pub SDLK_T               = 116,
+        pub SDLK_U               = 117,
+        pub SDLK_V               = 118,
+        pub SDLK_W               = 119,
+        pub SDLK_X               = 120,
+        pub SDLK_Y               = 121,
+        pub SDLK_Z               = 122,
+        pub SDLK_DELETE          = 127,
+        /* End of ASCII mapped KeySyms */
+        /*@}*/
+
+        /** @name International keyboard syms */
+        /*@{*/
+        pub SDLK_WORLD0          = 160,      /* 0xA0 */
+        pub SDLK_WORLD1          = 161,
+        pub SDLK_WORLD2          = 162,
+        pub SDLK_WORLD3          = 163,
+        pub SDLK_WORLD4          = 164,
+        pub SDLK_WORLD5          = 165,
+        pub SDLK_WORLD6          = 166,
+        pub SDLK_WORLD7          = 167,
+        pub SDLK_WORLD8          = 168,
+        pub SDLK_WORLD9          = 169,
+        pub SDLK_WORLD10         = 170,
+        pub SDLK_WORLD11         = 171,
+        pub SDLK_WORLD12         = 172,
+        pub SDLK_WORLD13         = 173,
+        pub SDLK_WORLD14         = 174,
+        pub SDLK_WORLD15         = 175,
+        pub SDLK_WORLD16         = 176,
+        pub SDLK_WORLD17         = 177,
+        pub SDLK_WORLD18         = 178,
+        pub SDLK_WORLD19         = 179,
+        pub SDLK_WORLD20         = 180,
+        pub SDLK_WORLD21         = 181,
+        pub SDLK_WORLD22         = 182,
+        pub SDLK_WORLD23         = 183,
+        pub SDLK_WORLD24         = 184,
+        pub SDLK_WORLD25         = 185,
+        pub SDLK_WORLD26         = 186,
+        pub SDLK_WORLD27         = 187,
+        pub SDLK_WORLD28         = 188,
+        pub SDLK_WORLD29         = 189,
+        pub SDLK_WORLD30         = 190,
+        pub SDLK_WORLD31         = 191,
+        pub SDLK_WORLD32         = 192,
+        pub SDLK_WORLD33         = 193,
+        pub SDLK_WORLD34         = 194,
+        pub SDLK_WORLD35         = 195,
+        pub SDLK_WORLD36         = 196,
+        pub SDLK_WORLD37         = 197,
+        pub SDLK_WORLD38         = 198,
+        pub SDLK_WORLD39         = 199,
+        pub SDLK_WORLD40         = 200,
+        pub SDLK_WORLD41         = 201,
+        pub SDLK_WORLD42         = 202,
+        pub SDLK_WORLD43         = 203,
+        pub SDLK_WORLD44         = 204,
+        pub SDLK_WORLD45         = 205,
+        pub SDLK_WORLD46         = 206,
+        pub SDLK_WORLD47         = 207,
+        pub SDLK_WORLD48         = 208,
+        pub SDLK_WORLD49         = 209,
+        pub SDLK_WORLD50         = 210,
+        pub SDLK_WORLD51         = 211,
+        pub SDLK_WORLD52         = 212,
+        pub SDLK_WORLD53         = 213,
+        pub SDLK_WORLD54         = 214,
+        pub SDLK_WORLD55         = 215,
+        pub SDLK_WORLD56         = 216,
+        pub SDLK_WORLD57         = 217,
+        pub SDLK_WORLD58         = 218,
+        pub SDLK_WORLD59         = 219,
+        pub SDLK_WORLD60         = 220,
+        pub SDLK_WORLD61         = 221,
+        pub SDLK_WORLD62         = 222,
+        pub SDLK_WORLD63         = 223,
+        pub SDLK_WORLD64         = 224,
+        pub SDLK_WORLD65         = 225,
+        pub SDLK_WORLD66         = 226,
+        pub SDLK_WORLD67         = 227,
+        pub SDLK_WORLD68         = 228,
+        pub SDLK_WORLD69         = 229,
+        pub SDLK_WORLD70         = 230,
+        pub SDLK_WORLD71         = 231,
+        pub SDLK_WORLD72         = 232,
+        pub SDLK_WORLD73         = 233,
+        pub SDLK_WORLD74         = 234,
+        pub SDLK_WORLD75         = 235,
+        pub SDLK_WORLD76         = 236,
+        pub SDLK_WORLD77         = 237,
+        pub SDLK_WORLD78         = 238,
+        pub SDLK_WORLD79         = 239,
+        pub SDLK_WORLD80         = 240,
+        pub SDLK_WORLD81         = 241,
+        pub SDLK_WORLD82         = 242,
+        pub SDLK_WORLD83         = 243,
+        pub SDLK_WORLD84         = 244,
+        pub SDLK_WORLD85         = 245,
+        pub SDLK_WORLD86         = 246,
+        pub SDLK_WORLD87         = 247,
+        pub SDLK_WORLD88         = 248,
+        pub SDLK_WORLD89         = 249,
+        pub SDLK_WORLD90         = 250,
+        pub SDLK_WORLD91         = 251,
+        pub SDLK_WORLD92         = 252,
+        pub SDLK_WORLD93         = 253,
+        pub SDLK_WORLD94         = 254,
+        pub SDLK_WORLD95         = 255,      /* 0xFF */
+        /*@}*/
+
+        /** @name Numeric keypad */
+        /*@{*/
+        pub SDLK_KP0             = 256,
+        pub SDLK_KP1             = 257,
+        pub SDLK_KP2             = 258,
+        pub SDLK_KP3             = 259,
+        pub SDLK_KP4             = 260,
+        pub SDLK_KP5             = 261,
+        pub SDLK_KP6             = 262,
+        pub SDLK_KP7             = 263,
+        pub SDLK_KP8             = 264,
+        pub SDLK_KP9             = 265,
+        pub SDLK_KPPERIOD        = 266,
+        pub SDLK_KPDIVIDE        = 267,
+        pub SDLK_KPMULTIPLY      = 268,
+        pub SDLK_KPMINUS         = 269,
+        pub SDLK_KPPLUS          = 270,
+        pub SDLK_KPENTER         = 271,
+        pub SDLK_KPEQUALS        = 272,
+        /*@}*/
+
+        /** @name Arrows + Home/End pad */
+        /*@{*/
+        pub SDLK_UP              = 273,
+        pub SDLK_DOWN            = 274,
+        pub SDLK_RIGHT           = 275,
+        pub SDLK_LEFT            = 276,
+        pub SDLK_INSERT          = 277,
+        pub SDLK_HOME            = 278,
+        pub SDLK_END             = 279,
+        pub SDLK_PAGEUP          = 280,
+        pub SDLK_PAGEDOWN        = 281,
+        /*@}*/
+
+        /** @name Function keys */
+        /*@{*/
+        pub SDLK_F1              = 282,
+        pub SDLK_F2              = 283,
+        pub SDLK_F3              = 284,
+        pub SDLK_F4              = 285,
+        pub SDLK_F5              = 286,
+        pub SDLK_F6              = 287,
+        pub SDLK_F7              = 288,
+        pub SDLK_F8              = 289,
+        pub SDLK_F9              = 290,
+        pub SDLK_F10             = 291,
+        pub SDLK_F11             = 292,
+        pub SDLK_F12             = 293,
+        pub SDLK_F13             = 294,
+        pub SDLK_F14             = 295,
+        pub SDLK_F15             = 296,
+        /*@}*/
+
+        /** @name Key state modifier keys */
+        /*@{*/
+        pub SDLK_NUMLOCK         = 300,
+        pub SDLK_CAPSLOCK        = 301,
+        pub SDLK_SCROLLOCK       = 302,
+        pub SDLK_RSHIFT          = 303,
+        pub SDLK_LSHIFT          = 304,
+        pub SDLK_RCTRL           = 305,
+        pub SDLK_LCTRL           = 306,
+        pub SDLK_RALT            = 307,
+        pub SDLK_LALT            = 308,
+        pub SDLK_RMETA           = 309,
+        pub SDLK_LMETA           = 310,
+        pub SDLK_LSUPER          = 311,      /**< Left "Windows" key */
+        pub SDLK_RSUPER          = 312,      /**< Right "Windows" key */
+        pub SDLK_MODE            = 313,      /**< "Alt Gr" key */
+        pub SDLK_COMPOSE         = 314,      /**< Multi-key compose key */
+        /*@}*/
+
+        /** @name Miscellaneous function keys */
+        /*@{*/
+        pub SDLK_HELP            = 315,
+        pub SDLK_PRINT           = 316,
+        pub SDLK_SYSREQ          = 317,
+        pub SDLK_BREAK           = 318,
+        pub SDLK_MENU            = 319,
+        pub SDLK_POWER           = 320,      /**< Power Macintosh power key */
+        pub SDLK_EURO            = 321,      /**< Some european keyboards */
+        pub SDLK_UNDO            = 322,      /**< Atari keyboard has Undo */
+        /*@}*/
+
+        /* Add any other keys here */
+
+        SDLK_LAST
+}
+
 pub mod video {
 
     type RWOps = c_void;
