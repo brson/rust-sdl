a2330db247581c3b9de0580befd005c5029cfba4
Patrick Walton <pcwalton@mimiga.net>
2013-01-12 18:46:11 -0800

    Add a safe locking function for direct pixel access

--- video.rs
+++ video.rs
@@ -1,12 +1,13 @@
+use sdl;
+use ll;
+use util::Rect;
+
 use core::result::{Result, Err, Ok};
+use core::cast::transmute;
 use core::cast;
 use core::libc::{c_void, c_int, c_char};
 use core::str;
 use core::vec;
-
-use sdl;
-use ll;
-use util::Rect;
 
 pub enum SurfaceFlag {
     SWSurface = 0x00000000,
@@ -29,9 +30,8 @@ pub struct Surface {
     priv raw_surface: *ll::video::SDL_Surface,
 }
 
-impl Surface {
-
-    fn display_format() -> Result<~Surface, ~str> {
+pub impl Surface {
+    fn display_format(&self) -> Result<~Surface, ~str> {
         let raw_surface = ll::video::SDL_DisplayFormat(self.raw_surface);
         if raw_surface == ptr::null() {
             Err(sdl::get_error())
@@ -40,40 +40,51 @@ impl Surface {
         }
     }
 
-    fn flip() -> bool {
+    fn flip(&self) -> bool {
         ll::video::SDL_Flip(self.raw_surface) == 0 as c_int
     }
 
-    fn lock() -> bool {
+    /// Locks a surface so that the pixels can be directly accessed safely.
+    fn with_lock<R>(&self, f: &fn(pixels: &mut [u8]) -> R) -> R {
+        unsafe {
+            if ll::video::SDL_LockSurface(self.raw_surface) != 0 { fail; }
+            let len = (*self.raw_surface).pitch as uint * ((*self.raw_surface).h as uint);
+            let pixels: &mut [u8] = transmute(((*self.raw_surface).pixels, len));
+            let rv = f(pixels);
+            if ll::video::SDL_UnlockSurface(self.raw_surface) != 0 { fail; }
+            rv
+        }
+    }
+
+    fn lock(&self) -> bool {
         return ll::video::SDL_LockSurface(self.raw_surface) == 0 as c_int;
     }
 
-    fn unlock() -> bool {
+    fn unlock(&self) -> bool {
         return ll::video::SDL_UnlockSurface(self.raw_surface) == 0 as c_int;
     }
 
-    fn blit_surface_rect(src: &Surface, srcrect: &Rect, dstrect: &Rect) -> bool {
+    fn blit_surface_rect(&self, src: &Surface, srcrect: &Rect, dstrect: &Rect) -> bool {
         let res = ll::video::SDL_UpperBlit(src.raw_surface, srcrect, self.raw_surface, dstrect);
         return res == 0 as c_int;
     }
 
-    fn blit_surface(src: &Surface) -> bool {
+    fn blit_surface(&self, src: &Surface) -> bool {
         let res = ll::video::SDL_UpperBlit(src.raw_surface, ptr::null(), self.raw_surface, ptr::null());
         return res == 0 as c_int;
     }
 
-    fn fill_rect(rect: &Rect, color: u32) -> bool {
+    fn fill_rect(&self, rect: &Rect, color: u32) -> bool {
         return ll::video::SDL_FillRect(self.raw_surface, rect, color) == 0 as c_int;
     }
 
-    fn fill(color: u32) -> bool {
+    fn fill(&self, color: u32) -> bool {
         return ll::video::SDL_FillRect(self.raw_surface, ptr::null(), color) == 0 as c_int;
     }
 }
 
-impl Surface : Drop {
-
-    fn finalize(&self) {
+pub impl Surface : Drop {
+    pub fn finalize(&self) {
         ll::video::SDL_FreeSurface(self.raw_surface)
     }
 }
