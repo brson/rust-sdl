b824b3df450482906beea86e6eba84802af204cf
Tony Aldridge <tony@dabapps.com>
2013-02-06 08:53:45 +0000

    Replaced oldschool type for struct

--- util.rs
+++ util.rs
@@ -1,12 +1,12 @@
 use sdl::InitFlag;
 use ll;
 
-pub type Rect = {
+pub struct Rect {
     x: i16,
     y: i16,
     w: u16,
     h: u16
-};
+}
 
 pub fn init_flags_to_bitfield(flags: &[InitFlag]) -> u32 {
     vec::foldl(0u32, flags, |flags, flag| {
