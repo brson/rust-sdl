9c02c3f7739cdd1fa4a58ccfede6590648c5df14
Robert Norris <rob@eatenbyagrue.org>
2013-04-10 08:59:28 +1000

    implement SDL_GL_* functions

---
+++ src/gl.rs
@@ -0,0 +1,42 @@
+use core::libc::{c_int};
+
+pub mod ll {
+    use core::libc::{c_int};
+
+    pub extern "C" {
+        fn SDL_GL_SetAttribute(attr: c_int, value: c_int) -> c_int;
+        fn SDL_GL_SwapBuffers();
+    }
+}
+
+pub enum GLAttribute {
+    RedSize = 0,
+    GreenSize,
+    BlueSize,
+    AlphaSize,
+    BufferSize,
+    DoubleBuffer,
+    DepthSize,
+    StencilSize,
+    AccumRedSize,
+    AccumGreenSize,
+    AccumBlueSize,
+    AccumAlphaSize,
+    Stereo,
+    MultiSampleBuffers,
+    MultiSampleSamples,
+    AcceleratedVisual,
+    SwapControl
+}
+
+pub fn set_attribute(attr: GLAttribute, value: int) -> int {
+    unsafe {
+        ll::SDL_GL_SetAttribute(attr as c_int, value as c_int) as int
+    }
+}
+
+pub fn swap_buffers() {
+    unsafe {
+        ll::SDL_GL_SwapBuffers();
+    }
+}
