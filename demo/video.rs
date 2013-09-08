6e32726aeb046d5f84ff8bbbaea36b3dc4286c18
Tony Aldridge <zaragopha@hotmail.com>
2013-09-08 11:23:00 +0100

    Removed unneccesary call to SDL::Start

--- demo/video.rs
+++ demo/video.rs
@@ -1,48 +1,47 @@
 use std::rand::RngUtil;
 use std::rand;
+use std::rt;
 
 use sdl;
 
 pub fn main() {
-	do sdl::start {
-		sdl::init([sdl::InitVideo]);
-		sdl::wm::set_caption("rust-sdl demo - video", "rust-sdl");
+    sdl::init([sdl::InitVideo]);
+    sdl::wm::set_caption("rust-sdl demo - video", "rust-sdl");
 
-		let mut rng = rand::rng();
-		let screen = match sdl::video::set_video_mode(800, 600, 32, [sdl::video::HWSurface],
-			                                                        [sdl::video::DoubleBuf]) {
-			Ok(screen) => screen,
-			Err(err) => fail!(fmt!("failed to set video mode: %s", err))
-		};
+    let mut rng = rand::rng();
+    let screen = match sdl::video::set_video_mode(800, 600, 32, [sdl::video::HWSurface],
+                                                                [sdl::video::DoubleBuf]) {
+        Ok(screen) => screen,
+        Err(err) => fail!(fmt!("failed to set video mode: %s", err))
+    };
 
-		// Note: You'll want to put this and the flip call inside the main loop
-		// but we don't as to not startle epileptics
-		for i in range(0, 10) {
-			for j in range(0, 10) {
-				screen.fill_rect(Some(sdl::Rect {
-					x: (i as i16) * 800 / 10,
-					y: (j as i16) * 600 / 10,
-					w: 800 / 10,
-					h: 600 / 10
-				}), rng.gen::<sdl::video::Color>());
-			}
-		}
+    // Note: You'll want to put this and the flip call inside the main loop
+    // but we don't as to not startle epileptics
+    for i in range(0, 10) {
+        for j in range(0, 10) {
+            screen.fill_rect(Some(sdl::Rect {
+                x: (i as i16) * 800 / 10,
+                y: (j as i16) * 600 / 10,
+                w: 800 / 10,
+                h: 600 / 10
+            }), rng.gen::<sdl::video::Color>());
+        }
+    }
 
-		screen.flip();
+    screen.flip();
 
-		'main : loop {
-			'event : loop {
-				match sdl::event::poll_event() {
-					sdl::event::QuitEvent => break 'main,
-					sdl::event::NoEvent => break 'event,
-					sdl::event::KeyEvent(k, _, _, _)
-					    if k == sdl::event::EscapeKey
-					        => break 'main,
-					_ => {}
-				}
-			}
-		}
+    'main : loop {
+        'event : loop {
+            match sdl::event::poll_event() {
+                sdl::event::QuitEvent => break 'main,
+                sdl::event::NoEvent => break 'event,
+                sdl::event::KeyEvent(k, _, _, _)
+                    if k == sdl::event::EscapeKey
+                        => break 'main,
+                _ => {}
+            }
+        }
+    }
 
-		sdl::quit();
-	}
+    sdl::quit();
 }
