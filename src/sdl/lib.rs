fdcd15d7930caf2362fca09cfc8c07163b992269
phil <phillip.walters@gmail.com>
2014-01-05 13:30:16 -0800

    Changed link to crate_id

--- src/sdl/lib.rs
+++ src/sdl/lib.rs
@@ -1,8 +1,4 @@
-#[link(name = "sdl",
-       vers = "0.3.0",
-       uuid = "0ce89b41-2f92-459e-bbc1-8f5fe32f16cf",
-       url = "http://github.com/brson/rust-sdl")];
-
+#[crate_id="sdl#0.3.1"];
 #[comment = "SDL bindings"];
 #[license = "MIT"];
 #[crate_type = "lib"];
